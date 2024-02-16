7bf9de097638502dd2863c50587f72e817683e0f
abc <abc@gmail.com>
2024-02-16 10:06:43 +0530

    FirstCommit

---
+++ Calc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalExample
+{
+    public class Calc
+    {
+        public void Add(double n1, double n2)
+        {
+            Console.WriteLine("Result after addition: \t" +(n1+n2));
+        }
+        public void Sub(double n1, double n2)
+        {
+            Console.WriteLine("Result after Subtration: \t" + (n1 - n2));
+        }
+        public void Div(double n1, double n2)
+        {
+            Console.WriteLine("Result after Division: \t" + (n1 / n2));
+        }
+        public void Multi(double n1, double n2)
+        {
+            Console.WriteLine("Result after Multiplication: \t" + (n1 * n2));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CalExample;
+Calc calc = new Calc();
+Console.WriteLine("Add Call");
+calc.Add(12, 45);
+
+
